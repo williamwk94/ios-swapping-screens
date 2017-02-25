ba1470bd0c863a4fc390059c185d47eb88414231
William Kurniawan <w.k.williamkurniawan@gmail.com>
2017-02-25 16:12:46 +1100

    Finish

---
+++ SwappingScreens/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by William Kurniawan on 25/2/17.
+//  Copyright © 2017 William Kurniawan. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
---
+++ SwappingScreens/MusicListVC.swift
@@ -0,0 +1,41 @@
+//
+//  MusicListVC.swift
+//  SwappingScreens
+//
+//  Created by William Kurniawan on 25/2/17.
+//  Copyright © 2017 William Kurniawan. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.blue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func load3rdScreenPressed(_ sender: Any) {
+        let songTitle = "Quit Playing Games with My Heart"
+        performSegue(withIdentifier: "PlaySongVC", sender: songTitle)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? PlaySongVC {
+            if let song = sender as? String {
+                destination.selectedSong = song
+            }
+        }
+    }
+
+}
